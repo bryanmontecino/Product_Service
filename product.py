b4fc8d1f43e60fa5bc55357cc77e4e6d1d54b4f3
Bryan Montecino <112580259+bryanmontecino@users.noreply.github.com>
2023-09-24 12:07:15 -0400

    Create product.py

---
+++ product.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Configuration for SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///kachowproducts.db'  # SQLite database file name
+db = SQLAlchemy(app)
+
+# Define a Product model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_list.append({
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'quantity': product.quantity
+        })
+    return jsonify(product_list)
+
+@app.route('/products/<int:product_id>', methods=['GET'])
+def get_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        product_data = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'quantity': product.quantity
+        }
+        return jsonify(product_data)
+    return jsonify({'message': 'Product not found'}), 404
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    new_product = Product(
+        name=data['name'],
+        price=data['price'],
+        quantity=data['quantity']
+    )
+    db.session.add(new_product)
+    db.session.commit()
+    return jsonify({
+        'id': new_product.id,
+        'name': new_product.name,
+        'price': new_product.price,
+        'quantity': new_product.quantity
+    }), 201
+
+if __name__ == '__main__':
+    #db.create_all()
+    app.run(host='127.0.0.1', port=5000)
+    #app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    #app.run(debug=True)
